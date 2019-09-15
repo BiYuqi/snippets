6df8b31f7d6dec6ee0bd5faa63e5b8a2279316cb
BiYuqi <biyuqiwan@163.com>
2019-09-15 22:44:56 +0800

    refactor: seperate dir

---
+++ web/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.scss'
+
+class App extends React.Component {
+  render(){
+    return (
+      <div className="magic-program">
+        <div className="magic-program_title">Magic Programming</div>
+      </div>
+    )
+  } 
+}
+
+export default App
