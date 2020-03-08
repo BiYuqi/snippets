7a8d1dc91bed23ab9c678999eb10d6b0f9655f63
BiYuqi <biyuqiwan@163.com>
2020-03-08 15:50:28 +0800

    chore: first commit

---
+++ src/fetchIssue.ts
@@ -0,0 +1,8 @@
+// import axios, { AxiosRequestConfig, AxiosPromise } from 'axios'
+
+interface Config {
+  owner: string,
+  repo: string,
+  token: string
+}
+
---
+++ src/index.ts
@@ -0,0 +1,2 @@
+console.log('I am form index.ts0000')
+console.log('测试222')
