48748fec63bfc653a11ce4b69a0120b91e043889
BiYuqi <biyuqiwan@163.com>
2020-03-08 17:38:13 +0800

    feat: refactor using typescript

--- src/fetchIssues.ts
+++ src/fetchIssues.ts
@@ -1,14 +1,42 @@
-// import axios, { AxiosRequestConfig, AxiosPromise } from 'axios'
-import { Issues } from "./simplifyIssue";
+import fetch from 'node-fetch'
+import { Issues } from "./simplifyIssue"
 
-interface Config {
-  owner: string
-  repo: string
+const COS_ENV = process.argv.slice(2)
+const [GITHUB_SNIPPET] = COS_ENV
+
+interface EnvConfig {
+  owner: string,
+  repo: string,
   token: string
 }
 
+const EnvConfig: EnvConfig = {
+  owner: 'BiYuqi',
+  repo: 'snippets',
+  token: GITHUB_SNIPPET
+}
+
+function githubIssueApi({owner, repo, token}): string {
+  return `https://api.github.com/repos/${owner}/${repo}/issues${generateQuery({owner, token})}`
+}
+
+function generateQuery({owner, token}): string {
+  return `?creator=${owner}&per_page=1000&access_token=${token}`
+}
+
+// const REDIRECT_URL = ({owner, repo}) => {
+//   return `https://github.com/${owner}/${repo}/issues`;
+// }
+
 export function fetchIssues(): Promise<Array<Issues>> {
   return new Promise((resolve, reject) => {
-    resolve([])
+    fetch(githubIssueApi(EnvConfig))
+      .then(res => res.json())
+      .then(json => {
+        resolve(json)
+      })
+      .catch(e => {
+        reject(e)
+      })
   })
 }
